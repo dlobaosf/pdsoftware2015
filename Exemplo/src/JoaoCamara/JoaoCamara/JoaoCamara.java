c83d9cbaac1f37e2b5250f54f4a3e034b91cc06e
20121054010061 <20121054010061@IP280609.ifrn.local>
2015-08-17 11:07:04 -0300

    Criado o pacote e classe com nome: "FranciscoKennedi"

--- Exemplo/src/JoaoCamara/JoaoCamara/JoaoCamara.java
+++ Exemplo/src/JoaoCamara/JoaoCamara/JoaoCamara.java
@@ -11,5 +11,7 @@ package JoaoCamara.JoaoCamara;
  * @author 20121054010347
  */
 public class JoaoCamara {
-    
+    public static void main(String []args){
+        System.out.println("Eu sou viadão!!!");
+    }
 }

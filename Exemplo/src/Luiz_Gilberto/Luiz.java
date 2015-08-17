6495a499fe8b25db5fb434a43cca92816f970fed
20121054010320 <20121054010320@ip280608.ifrn.local>
2015-08-17 10:41:24 -0300

    Criado o pacote Luiz_Gilberto.

---
+++ Exemplo/src/Luiz_Gilberto/Luiz.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Luiz_Gilberto;
+
+/**
+ *
+ * @author 20121054010320
+ */
+public class Luiz {
+    
+}
